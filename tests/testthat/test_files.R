abb3ae74e1977140b2c205351292246721d6ded2
Hannes Muehleisen <hannes@muehleisen.org>
2016-09-01 16:12:03 +0200

    fixses

---
+++ tests/testthat/test_files.R
@@ -0,0 +1,28 @@
+library(testthat)
+
+test_that("it parses our ugly collection", {
+	dists <- parallel::mclapply(dir("../data/original", full.names=TRUE, recursive=TRUE), function(f) {
+		print(f)
+		clean_table = feather::read_feather(paste0("../data/cleaned/",basename(f),".feather"))
+
+		res <- hypoparsr::parse_file(f)
+		print(res)
+
+		parsed_table = as.data.frame(res)
+		clean_string=paste(as.character(unlist(clean_table)),collapse=" ")
+		if(any(colnames(clean_table)!="")){
+			clean_string = paste(paste(colnames(clean_table),collapse=" "),clean_string)
+		}
+
+	    parsed_string = paste(as.character(unlist(parsed_table)),collapse=" ")
+	    if(any(colnames(parsed_table)!="")){
+	        parsed_string = paste(paste(colnames(parsed_table),collapse=" "),parsed_string)
+	    }
+
+	   dist = RecordLinkage::levenshteinDist(parsed_string,clean_string)
+	   print(dist)
+	   dist
+	})
+	
+})
+
